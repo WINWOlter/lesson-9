599eebd559b9f9f56050a32ab19bcd98439d61dd
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-03-19 08:04:37 +0000

    добавив ціну ефіра

--- main.py
+++ main.py
@@ -11,5 +11,11 @@ if res.status_code == 200:
   price = info[0].getText()
   print(price)
 
+if res.status_code == 200:
+  soup = BeautifulSoup(res.text,  features ="html.parser")
+  info = soup.find_all("a", {"href":"/currencies/ethereum/markets/"})
+  price = info[0].getText()
+  print(price)
+
 
 
