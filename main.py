accfdc3bfb4f705a791d4300e7f8fde1cef8f0ef
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-03-19 07:38:18 +0000

    вибиваєм ціну біткойна

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = "https://www.python.org/"
+URL = "https://coinmarketcap.com/"
 
 res = requests.get(URL)
 
-print(res.text)
+if res.status_code == 200:
+  soup = BeautifulSoup(res.text,  features ="html.parser")
+  info = soup.find_all("a", {"href":"/currencies/bitcoin/markets/"})
+  print(info[0])
+
+
+
